e2455855372f34d7f3704e9ab04b6a27c2ccd02a
Tony Zheng <tony6847310@gmail.com>
2017-08-07 01:16:48 +0800

    Add GM mode

--- kk/js/gamble_js.js
+++ kk/js/gamble_js.js
@@ -1,8 +1,12 @@
 // Get the element
 var dbRefAdvice = firebase.database().ref('advices/').child('advice');
+var dbRefAnswer = firebase.database().ref().child('answer');
 
 // Other variables
 var optionCounter = 2;
+var curAnswer = null;
+var answerMatched = false;
+var GMmode = false;
 
 $('#add').click(function(){
     // pass the value to database
@@ -33,7 +37,52 @@ dbRefAdvice.on('child_added', function (snapchat) {
 function focusOption(num){
     $(".gamble-options button").css("border", "1px solid green");
     $("#btn"+num).css("border", "2px solid red");
+    if (GMmode) {
+        var newAnswer = dbRefAnswer.push();
+        newAnswer.set({
+            content: num
+        });
+    } else {
+        if(num == curAnswer) answerMatched = true;
+        else answerMatched = false;
+    }
 }
 
-// TEST: refresh gamble table
-dbRefAdvice.remove();
+// get answer from database
+dbRefAnswer.on('child_added', function (snapchat) {
+    curAnswer = snapchat.val.content;
+});
+
+// Wizard of Oz: refresh gamble table
+$('#mode').click(function(){
+    if(GMmode) {
+        $('#mode').text("GM mode OFF");
+        GMmode = false;
+    } else {
+        $('#mode').text("GM mode ON");
+        GMmode = true;
+    }
+});
+
+function refreshGamble(){
+    $(".gamble-options button").remove();
+}
+
+$('#reset').click(function(){
+    dbRefAdvice.remove();
+    refreshGamble();
+});
+
+// pop result announcement window
+dbRefAdvice.on('value', function (snapchat){
+    if (!GMmode) {
+        if(!snapchat.exists() && answerMatched) {
+            alert("YOU WIN!!!");
+            answerMatched = false;
+            refreshGamble();
+        } else if(!snapchat.exists() && !answerMatched) {
+            alert("You lose...")
+            refreshGamble();
+        }
+    }
+});
